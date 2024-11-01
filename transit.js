0ab28245601bd4a906b3e66fdde0aec51abc78a1
Botaro Shinomiya <nothing@citri.one>
2024-11-02 00:24:39 +0300

    add: transit.js

---
+++ transit.js
@@ -0,0 +1,23 @@
+/*
+    Lusk, a modular plugin-based project manager.
+    Open-Source, MIT License
+
+    Lusk Builtins, Built-in Transits that serve
+    basic operations for Lusk.
+
+    Copyright (C) 2024 Botaro Shinomiya <nothing@citri.one>
+    Copyright (C) 2024 OSCILLIX <oscillixonline@gmail.com>
+    Copyright (C) 2024 Bluskript <bluskript@gmail.com>
+    Copyright (C) 2024 N1kO23 <niko.huuskonen.00@gmail.com>
+
+    Given copyright notes are for exclusive rights to go
+    beyond the license's limits. For more information, please
+    check https://github.com/luskproject/lusk-builtins/
+*/
+
+Transit( {
+    id: "LuskBuiltins",
+    plugin ( Manager ) {
+
+    }
+} );
